0791b8f2403563922e48748cfe084cd08b30fd4d
Taras <snoopgtg@gmail.com>
2018-06-01 00:05:31 +0300

    super hero class

---
+++ src/main/java/com/game/swingy/core/Heros.java
@@ -0,0 +1,15 @@
+package com.game.swingy.core;
+
+public class Heros {
+
+    protected int id;//TODO як краще визначати героя та ворога
+    protected String name;
+    protected String heroClass;
+    protected int level;
+    protected int experience;
+    protected int attack;
+    protected int defense;
+    protected int hitPoints;
+
+    private static int idCounter; //для унікальності id
+}
--- src/main/java/com/game/swingy/core/heros.java
+++
@@ -1,6 +0,0 @@
-package com.game.swingy.core;
-
-public class heros {
-
-
-}
