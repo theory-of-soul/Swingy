a11e4413a4a64d405dc646158ac6b6a331648ebd
Taras <snoopgtg@gmail.com>
2018-06-02 16:02:06 +0300

    refactor view and controller connections

--- src/main/java/com/game/swingy/controller/StarterController.java
+++ src/main/java/com/game/swingy/controller/StarterController.java
@@ -3,12 +3,20 @@ package com.game.swingy.controller;
 import com.game.swingy.view.gui.CreateHeroView;
 import com.game.swingy.view.gui.StartView;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class StarterController {
 
-    StartView startView = new StartView(this);
+    StartView startView = new StartView();
 
     public void startGame() {
 
+        startView.getBtnCreateHero().addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onClickCreateHero();
+            }
+        });
         startView.show();
     }
 
--- src/main/java/com/game/swingy/view/gui/StartView.java
+++ src/main/java/com/game/swingy/view/gui/StartView.java
@@ -9,31 +9,18 @@ import java.awt.event.*;
 public class StartView {
 
     private JFrame jf;
+    private JButton btnCreateHero;
+    private JButton btnPreviouslyHero;
 
-    StarterController starterController;
-
-    public StartView(StarterController starterController) {
-
-        this.starterController = starterController;
-
+    public StartView() {
+        this.btnCreateHero = new JButton("Create a hero");//Creating a Button named Say Hello
+        this.btnPreviouslyHero = new JButton("Select a previously created hero");//Creating a Button named Say Hello
     }
+
     public void show() {
 
         jf = new JFrame("Swingy");		//Creating a JFrame with name MyWindow
-        JButton btnCreateHero = new JButton("Create a hero");//Creating a Button named Say Hello
-        btnCreateHero.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                starterController.onClickCreateHero();
-            }
 
-        });
-        JButton btnPreviouslyHero = new JButton("Select a previously created hero");//Creating a Button named Say Hello
-        btnPreviouslyHero.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                starterController.onClickPreviouslyHero();
-            }
-
-        });
         jf.add(btnCreateHero);             		//adding button to frame
         jf.add(btnPreviouslyHero);             		//adding button to frame
         jf.setLayout(new FlowLayout());        //setting layout using FlowLayout object
@@ -42,4 +29,27 @@ public class StartView {
         jf.setVisible(true);            	//setting frame visibility
     }
 
+    public JFrame getJf() {
+        return jf;
+    }
+
+    public void setJf(JFrame jf) {
+        this.jf = jf;
+    }
+
+    public JButton getBtnCreateHero() {
+        return btnCreateHero;
+    }
+
+    public void setBtnCreateHero(JButton btnCreateHero) {
+        this.btnCreateHero = btnCreateHero;
+    }
+
+    public JButton getBtnPreviouslyHero() {
+        return btnPreviouslyHero;
+    }
+
+    public void setBtnPreviouslyHero(JButton btnPreviouslyHero) {
+        this.btnPreviouslyHero = btnPreviouslyHero;
+    }
 }
